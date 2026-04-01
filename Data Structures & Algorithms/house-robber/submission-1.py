5c6ad7ec28ff4eebaacdda4419bc8035601004ce
yenjnr <yenjnr@gmail.com>
2026-04-01 10:57:10 +0200

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+
+        arr = [nums[0], max(nums[0], nums[1])]
+        for i in range(2, len(nums)):
+            arr.append(max(arr[i - 2] + nums[i], arr[i - 1]))
+
+        return arr[-1]
