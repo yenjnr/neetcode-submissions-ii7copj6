fae5845ef39e5daa988e41ba199268e06488f432
yenjnr <yenjnr@gmail.com>
2026-04-10 10:27:40 +0200

    Remove: count-paths - submission-0

--- Data Structures & Algorithms/count-paths/submission-0.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        cache = [1] * n
-        for i in range(m - 2, -1, -1):
-            for j in range(n - 2, -1, -1):
-                cache[j] += cache[j + 1]
-        return cache[0]
