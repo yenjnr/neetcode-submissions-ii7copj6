2768c714a7f0f5b1764940960d2220b4dcd2d993
yenjnr <yenjnr@gmail.com>
2026-04-14 10:30:16 +0200

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1
+
+        for num in nums:
+            next_dp = defaultdict(int)
+            for total, count in dp.items():
+                next_dp[total + num] += count
+                next_dp[total - num] += count
+            dp = next_dp
+
+        return dp[target]
