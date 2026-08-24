086f7231013df652800e3953bf26da35be2cd7c5
yenjnr <yenjnr@gmail.com>
2026-08-24 09:43:15 +0200

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        int left = 0, right = matrix[0].size();
+        int top = 0, bottom = matrix.size();
+
+        while (left < right && top < bottom) {
+            for (int i = left; i < right; i++) {
+                res.push_back(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i < bottom; i++) {
+                res.push_back(matrix[i][right - 1]);
+            }
+            right--;
+            if (!(left < right && top < bottom)) {
+                break;
+            }
+            for (int i = right - 1; i >= left; i--) {
+                res.push_back(matrix[bottom - 1][i]);
+            }
+            bottom--;
+            for (int i = bottom - 1; i >= top; i--) {
+                res.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+
+        return res;
+    }
+};
